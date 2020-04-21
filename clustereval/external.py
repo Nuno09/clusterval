23ac378aba2606c98ca2f6f859d4947bc4b5f505
Nuno09 <nuno.mcs96@gmail.com>
2020-04-21 18:03:34 +0200

    added more indexes

--- clustereval/external.py
+++ clustereval/external.py
@@ -6,7 +6,6 @@ Calculate external validation validclust
 
 
 def calculate(partition_a, partition_b):
-
     import math
     import numpy as np
 
@@ -18,13 +17,14 @@ def calculate(partition_a, partition_b):
     # fill the contigency table
     for i in range(0, R + 1):
         for j in range(0, C):
-            if (i in range(0, R)):
+            if i in range(0, R):
                 n_common_elements = len(set(partition_a[i]).intersection(partition_b[j]))
                 ct[i][j] = n_common_elements
             else:
                 ct[i][j] = ct[:, j].sum()
 
         ct[i][j + 1] = ct[i].sum()
+
     N = ct[R][C]
     # condensed information of ct into a mismatch matrix (pairwise agreement)
     sum_all_squared = np.sum(ct[0:R][:, range(0, C)] ** 2)
@@ -34,18 +34,9 @@ def calculate(partition_a, partition_b):
     sum_C = np.sum(ct[R, 0:C])
     # computing the number of pairs that are in the same cluster both in partition A and partition B
     a = 0
-    VD_i = 0
-    VD_j = 0
-    flag_columns = 0
     for i in range(0, R):
-        VD_i += max(ct[i, 0:C])
         for j in range(0, C):
-            if flag_columns == 0:
-                VD_j += max(ct[0:R, j])
-
             a = a + ct[i][j] * (ct[i][j] - 1)
-
-        flag_columns = 1
 
     a = a / 2
     # computing the number of pair in the same cluster in partition A but in different cluster in partition B
@@ -55,6 +46,7 @@ def calculate(partition_a, partition_b):
     # computing the number of pairs in different cluster both in partition A and partition B
     d = (N ** 2 + sum_all_squared - (sum_R_squared + sum_C_squared)) / 2
 
+    M = (a + b + c + d)
     # Rand Index
     rand_index = (a + d) / (a + b + c + d)
 
@@ -89,49 +81,53 @@ def calculate(partition_a, partition_b):
     else:
         adjusted_wallace = (wallace - (1 - SID_B)) / (1 - (1 - SID_B))
 
-    # Van Dongen
-    van_dongen = (2 * N - VD_i - VD_j)
+    # Van Dongen ------> low value is best
+    VD_i = 0
+    VD_j = 0
 
-    hub = huberts(partition_a, partition_b) / (a + b + c + d)
+    for i in range(0, R):
+        VD_i += max(ct[i, 0:C])
+    for j in range(0, C):
+        VD_j += max(ct[0:R, j])
 
-    return [rand_index, adjusted_rand_index, FM, jaccard, adjusted_wallace, van_dongen, hub]
+    van_dongen = ((2*N) - VD_i - VD_j) / (2*N)
 
+    #Huberts statistic
+    hubert = (M - 2*b - 2*c) / M
 
-def huberts(C, P):
-    import itertools
+    #Huberts statistics norrmalized
+    aux_hub1 = (a+b)*(a+c)
+    aux_hub2 = (a+b)*(a+c)*(d+b)*(d+c)
+    hub_normalized = (M*a - aux_hub1) / (math.sqrt(aux_hub2))
 
-    # join clusters in one to find possible pair combinations
-    concat_cluster = list(itertools.chain.from_iterable(C))
-    pairs = list(itertools.combinations(concat_cluster, 2))
+    #F-Measure
+    f_measure = 2*a / (2*a + b + c)
 
-    X = build_hubert_matrix(C, pairs)
-    Y = build_hubert_matrix(P, pairs)
+    #VI ----> FIX
+    mutual_info = 0
+    entropy_C = 0
+    entropy_P = 0
+    flag_entropy = 0
+    for i in range(R):
+        # calculation for entropy
+        p_i = ct[i][-1] / N
+        if p_i != 0:
+            entropy_C = entropy_C + (p_i * math.log2(p_i))
+        for j in range(C):
+            if flag_entropy == 0:
+                # calculation for entropy
+                p = ct[-1][j] / N
+                if p!= 0:
+                    entropy_P = entropy_P + (p * math.log2(p))
+            p_ij = ct[i][j] / N
+            p_j = ct[-1][j] / N
+            if not 0 in [p_ij, p_i, p_j]:
+                mutual_info = mutual_info + (p_ij * math.log2(p_ij / (p_i*p_j)))
+        flag_entropy = 1
 
-    index = 0
-    for pair in pairs:
-        index = index + (X[pair] * Y[pair])
+    VI = -entropy_C - entropy_P - (2*mutual_info)
 
-    return index
-
-
-def build_hubert_matrix(C, pairs):
-    from collections import defaultdict
-
-    X = defaultdict(tuple)
-
-    # transform list of clusters in dictionary of clusters
-    dict_clusters = {i: c for i, c in enumerate(C)}
-
-    #X(i,j) = 1 if both in the same cluster, 0 otherwise
-    for el in pairs:
-        for k, v in dict_clusters.items():
-            if (el[0] in v) and (el[1] in v):
-                X[el] = 1
-        if not X.get(el, False):
-            X[el] = 0
-
-    return X
-
+    return [rand_index, adjusted_rand_index, FM, jaccard, adjusted_wallace, van_dongen, hubert, hub_normalized, f_measure, VI]
 
 
 
@@ -140,10 +136,11 @@ if __name__ == '__main__':
     from collections import defaultdict
     from statistics import mean
     from tabulate import tabulate
+    import math
 
 
     dicio_statistics = defaultdict(str)
-    indexes = ('rand', 'adjusted', 'FM', 'jaccard', 'adjusted_wallace', 'van_dongen', 'huberts')
+    indexes = ('rand', 'adjusted', 'FM', 'jaccard', 'adjusted_wallace', 'van_dongen', 'huberts', 'huberts_normalized', 'F-Measure', 'VI')
 
     for index in indexes:
         dicio_statistics[index] = []
